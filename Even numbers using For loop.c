e1f4871bbf671e85a037b6d32aa194ccd36561ca
bhupendra singh <bhupendracharan228@gmail.com>
2026-09-18 22:33:04 +0530

    Create Even numbers using For loop.c

---
+++ Even numbers using For loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+     for(int i=1;i<=100;i++){ // even number between 1 to 100 , with the help of using for loop.
+     if(i%2==0) 
+       
+            printf("%d\n",i);
+    }
+  
+    return 0;
+}
