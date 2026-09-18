75b8e3292ebfcccd6510d31f6abac900d57a284a
bhupendra singh <bhupendracharan228@gmail.com>
2026-09-18 22:37:53 +0530

    Create Odd numbers using For loop.c

---
+++ Odd numbers using For loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+     for(int i=1;i<=100;i++){   // using  for loop , to calculate numbers between 1 to 100
+       
+     if(i%2!=0)               //calculation of  odd numbers between 1 to 100 with the help of using for loop.                              
+            printf("%d\n",i);   
+    }
+  
+    return 0;
+}
