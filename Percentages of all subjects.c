8996727c6e62ce7d4f9902f575a6fbe677e536a2
bhupendra singh <bhupendracharan228@gmail.com>
2026-09-19 00:20:45 +0530

    Create Percentages of all subjects.c

---
+++ Percentages of all subjects.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() { 
+    float m1 = 85;   // marks of each subjects 
+    float m2 = 97;  
+    float m3 = 95; 
+    float m4 = 83; 
+    float m5 = 72;
+    float p=(m1+m2+m3+m4+m5);
+    printf("percentage of 5 subjects is : %f",p);
+    return 0;
+}
